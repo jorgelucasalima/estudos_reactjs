fec141823c5cf4bd4b9a8eeb9c4c650815ae236d
jorgelucasalima <jorgelucasalima@gmail.com>
2021-05-23 21:45:21 -0300

    aprendendo função

--- script.js
+++ script.js
@@ -3,7 +3,28 @@
 
 /*
 esse é meu nome
-
 */
 
+//função Entrar
 
+function entrar() {
+    var area = document.getElementById('area');
+    var texto = prompt("Digite Seu nome : ");
+
+    if (texto == '' || texto == null) {
+        alert("Digite seu nome novamente..")
+        area.innerHTML = 'Bem vindo .. '
+    }else{
+        area.innerHTML = 'Bem vindo ' + texto
+    }
+
+    
+}
+
+function entrar2(nome) {
+    var area = document.getElementById('area2')
+    var texto = prompt('Digite seu Sobrenome : ')
+    var idade = prompt('Digite sua Idade : ')
+
+    area.innerHTML = 'Seu nome é ' + nome + " " + texto + " " + idade
+}
